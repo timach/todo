07e9cc5b4cadb91eb68a49c8e71713d957d3ea1e
Hamtim <79050077888@ya.ru>
2023-04-26 00:18:12 +0500

    add Tasks controller

---
+++ app/controllers/TasksController.php
@@ -0,0 +1,18 @@
+<?php 
+
+namespace App\Controllers;
+
+class TasksController
+{
+    public function createAction(){
+
+    }
+
+    public function editAction(){
+        
+    }
+    
+    public function indexAction(){
+        echo 'task controler - index action';
+    }
+}
