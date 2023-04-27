6e384fd4634a4703fcd9b91aeb3f253af7e57081
Hamtim <79050077888@ya.ru>
2023-04-27 22:27:56 +0500

    config

--- app/config/mysql.php
+++ app/config/mysql.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    "host" => "31.31.196.203", 
-    "user" => "u0365384_todo", 
-    "password" => "sU3xZ7fE2hfH1tJ6", 
-    "dbname" => "u0365384_todo",
-    "dsn" => "mysql:host=31.31.196.203;dbname=u0365384_todo;charset=utf8mb4"
+    "host" => "", 
+    "user" => "", 
+    "password" => "", 
+    "dbname" => "",
+    "dsn" => ""
 ];
